750db85405b69590a8049362b9c85a91938771b6
waziri-cod <wazirishaban20@gmail.com>
2025-08-11 00:07:48 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,20 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+print(my_list.index(30))
+
+my_list = []
+
+
